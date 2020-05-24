8c90887b132f9ff5260a15f912410841685742bc
Vishwanath Sastry <vishu.dbz@gmail.com>
2020-05-24 20:53:23 +0530

    Replicated TodoMVC's checkbox styles

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,14 +3,18 @@ import '../styles/TodoItem.css';
 
 export const TodoItem = props => {
   const { complete, text } = props.todo;
+  console.log(complete);
   return (
     <div className="todo-item">
-      <p
-        onClick={props.toggleCompleted}
-        className={complete ? 'todo-complete todo-text' : 'todo-text'}
-      >
+      <input
+        onChange={props.toggleCompleted}
+        className="toggle"
+        type="checkbox"
+        checked={complete}
+      />
+      <label className={complete ? 'todo-complete-label' : 'todo-text-label'}>
         {text}
-      </p>
+      </label>
       <button onClick={props.deleteTodo} className="delete-btn">
         X
       </button>
